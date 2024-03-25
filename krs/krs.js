4fe32ca4649ae7fb1e90102ad1d7f7d3ac538475
unknown <ardiansyahrestu713@gmail.com>
2024-03-25 21:10:49 +0700

    melengkapi krs

---
+++ krs/krs.js
@@ -0,0 +1,28 @@
+function toggleDetails() {
+     var details = document.querySelector('.bg-matkul');
+     var button = document.querySelector('button');
+     if (details.style.display === 'none') {
+       details.style.display = 'block';
+       button.innerHTML = '<i class="fas fa-chevron-up"></i>'; // Mengubah teks dan ikon tombol
+       details.scrollIntoView({ behavior: "smooth", block: "start" });
+      } else {
+       details.style.display = 'none';
+       button.innerHTML =  '<i class="fas fa-chevron-down"></i>'; // Mengubah teks dan ikon tombol
+     }
+   }
+
+//    SMT 1
+function toggleDetailsSatu() {
+  var details = document.querySelector('.bg-matkul-1');
+  var button = document.querySelector('.toggle-button-krs-smt-1');
+  if (details.style.display === 'none') {
+    details.style.display = 'block';
+    button.innerHTML = '<i class="fas fa-chevron-up"></i>'; // Mengubah teks dan ikon tombol
+    
+    // Animasi scroll smooth
+    details.scrollIntoView({ behavior: "smooth", block: "start" });
+  } else {
+    details.style.display = 'none';
+    button.innerHTML =  '<i class="fas fa-chevron-down"></i>'; // Mengubah teks dan ikon tombol
+  }
+}
